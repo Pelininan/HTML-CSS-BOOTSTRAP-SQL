462e476b368f4b02e10ae914fb8458cb0fb4cdb6
Pelininan <71101949+Pelininan@users.noreply.github.com>
2022-03-31 14:47:00 +0300

    HTML-CSS-BOOTSTRAP-SQL
    freelancer-tele-marketing

---
+++ 1800003383-Pelinİnan-1800003553-PervinÖztürk/1800003383-Pelinİnan-1800003553-PervinÖztürk/Freelancer-Tele-marketing/SalesSiteProject/SalesSiteProject/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿
+
+using SalesSiteProject.Models;
+using System.Web.Mvc;
+
+namespace SalesSiteProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("Products")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+        [Route("Store")]
+        public ActionResult Store()
+        {
+            return View();
+        } 
+
+        [Route("Communication")]
+        public ActionResult Communication()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Communication(FormCollection form)
+        {
+            
+            SalessSiteProjectDBEntities db = new SalessSiteProjectDBEntities();
+            FormDb model = new FormDb();
+            model.City = form["city"].Trim();
+            model.UserName = form["name"].Trim();
+            model.UserEmail = form["email"].Trim();
+            model.PhoneNumber = form["phoneNumber"].Trim();
+            model.Product = form["product"].Trim();
+            model.Message = form["message"].Trim();
+            db.FormDbs.Add(model);
+            db.SaveChanges();
+      
+            
+
+            return View();
+        }
+
+    
+    }
+}
